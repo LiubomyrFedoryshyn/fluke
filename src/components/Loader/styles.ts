75976fe12019563bf0d61fb2250d02ba0f76ad2b
Любомир Федоришин <liubomyr.fedoryshyn@dataart.com>
2021-10-20 23:39:15 +0300

    Fix preloader on scroll

--- src/components/Loader/styles.ts
+++ src/components/Loader/styles.ts
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import { whiteColor } from "../../utils/colors";
 
 const SLoader = styled.div({
-    position: "absolute",
+    position: "fixed",
     top: 0,
     left: 0,
     width: "100vw",
-    height: "100vh",
+    height: "100%",
     opacity: "0.7",
     backgroundColor: whiteColor,
     zIndex: 2,
